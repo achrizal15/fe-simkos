ff7efeaf5e46bb760a0c6bdc6b8a9ad7da092c37
ACH RIZAL <achrizal15@gmail.com>
2023-07-23 15:19:34 +0700

    banner done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,48 @@
+import Image from 'next/image';
 import * as React from 'react';
-// https://sailing.thimpress.com/demo-2/
+import styles from './page.module.scss';
+import { ALBERT_SANS_500 } from 'constant/fonts';
+// https://sailing.thimpress.com/demo-1/
 export default function Page() {
-    return <div className='min-h-screen'>HOEEM</div>;
+    return (
+        <>
+            <section className={styles['banner-container']}>
+                <div className={`${styles.banner} ${styles.desktop}`}>
+                    <Image
+                        src={'/templates/beta/banner.jpg'}
+                        fill={true}
+                        objectFit='cover'
+                        alt='banner'
+                        priority
+                    />
+                </div>
+                <div className={`${styles.banner} ${styles.mobile}`}>
+                    <Image
+                        src={'/templates/beta/mobile.jpg'}
+                        fill={true}
+                        alt='banner'
+                        priority
+                    />
+                </div>
+
+                <div className={styles.content}>
+                    <div className={styles.wrapper}>
+                        <h1 className={ALBERT_SANS_500.className}>
+                            Welcome To Renggani Kost
+                        </h1>
+                        <p>
+                            Easy and practical, you can book boarding houses
+                            online through our platform without having to bother
+                            coming in person
+                        </p>
+                        <a href='tel:+6285234104446' target='_blank'>
+                            <button className={ALBERT_SANS_500.className}>
+                                Book Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
