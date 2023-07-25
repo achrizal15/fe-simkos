dc1ebb0605f0094409b9c056a303652b26039fa6
ACH RIZAL <achrizal15@gmail.com>
2023-07-25 08:56:57 +0700

    component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Section from '@/components/core/Section/Section';
-import Banner from './components/Banner';
+import Banner from './_components/Banner';
 
 // https://sailing.thimpress.com/demo-1/
 
