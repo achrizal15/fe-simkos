5df978feb3b742f7e87bc44ca4026e83403910bb
ACH RIZAL <achrizal15@gmail.com>
2023-07-27 19:35:22 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,13 @@ export default function Page() {
                 />
                 <section className='grid md:grid-cols-3 grid-cols-2 gap-2 '>
                     {data.map((e, k) => (
-                        <div className='h-80 bg-red-500 relative'>
+                        <div className='h-80 relative'>
                             <Image
                                 src={e.image}
                                 alt={e.name}
                                 key={e.code}
                                 fill
+                                loading='lazy'
                                 sizes='(max-width: 768px) 100vw'
                             />
                         </div>
