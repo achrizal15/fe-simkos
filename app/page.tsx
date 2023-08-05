59791869d7b7e8df38ee0890777c00d1c6162f28
ACH RIZAL <achrizal15@gmail.com>
2023-08-06 06:36:02 +0700

    Card Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,24 @@
 import * as React from 'react';
 import Section from '@/components/core/Section/Section';
 import Banner from './_components/Banner';
-import data from 'public/templates/beta/data/rooms.json';
-import Image from 'next/image';
+import RoomsSection from './_components/RoomsSection';
+
 // https://sailing.thimpress.com/demo-1/
 
 export default function Page() {
+
     return (
         <>
             <Banner />
-            <Section>
+            <Section >
                 <Section.Heading
                     title='Hotel Master Rooms'
                     description='Contrary to popular belief'
                 />
-                <section className='grid md:grid-cols-3 grid-cols-2 gap-2 '>
-                    {data.map((e, k) => (
-                        <div className='h-80 relative'>
-                            <Image
-                                src={e.image}
-                                alt={e.name}
-                                key={e.code}
-                                fill
-                                loading='lazy'
-                                sizes='(max-width: 768px) 100vw'
-                            />
-                        </div>
-                    ))}
-                </section>
+                <RoomsSection/>
+            </Section>
+            <Section>
+                Tess
             </Section>
         </>
     );
