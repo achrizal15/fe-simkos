cc182a004d899bdbbd2d24398b23d51dbe02f2c3
RIZAL <achrizal15@gmail.com>
2023-08-19 21:21:21 +0700

    env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,7 @@ const handler = NextAuth({
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials) {
-                    const res = await fetch(`http://127.0.0.1:8000/api/login`, {
+                    const res = await fetch(process.env.NEXT_PUBLIC_API_URL, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
