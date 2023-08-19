6285847d845b8bead1a5446f39d518cbe7065f3d
RIZAL <achrizal15@gmail.com>
2023-08-19 20:38:46 +0700

    lupa

--- app/rgpanel/page.tsx
+++ app/rgpanel/page.tsx
@@ -1,8 +1,10 @@
-
+'use client'
 import MobileMenu from "./_components/MobileMenu"
 import { useSession } from "next-auth/react"
 
 const Page = () => {
+    const session = useSession()
+    console.log(session)
     return (
         <div>
             <MobileMenu />
