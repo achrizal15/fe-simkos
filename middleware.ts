498b0a2e2852237c123a3a706a59e20403c3da37
RIZAL <achrizal15@gmail.com>
2023-08-25 13:37:22 +0700

    testing remove middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request })
-    if (request.nextUrl.pathname.startsWith('/login')) {
-        if(token!=null){
-            return NextResponse.redirect(new URL('/rgpanel', request.url))
-        }
-    }
-    if (request.nextUrl.pathname.startsWith('/rgpanel')) {
-        if(token==null){
-            return NextResponse.redirect(new URL('/login', request.url))
-        }
-    }
-}
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(request: NextRequest) {
+//     const token = await getToken({ req: request })
+//     if (request.nextUrl.pathname.startsWith('/login')) {
+//         if(token!=null){
+//             return NextResponse.redirect(new URL('/rgpanel', request.url))
+//         }
+//     }
+//     if (request.nextUrl.pathname.startsWith('/rgpanel')) {
+//         if(token==null){
+//             return NextResponse.redirect(new URL('/login', request.url))
+//         }
+//     }
+// }
