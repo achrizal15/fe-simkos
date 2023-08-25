1001564014f0a94e9a3deb753c8c20566fa85f68
RIZAL <achrizal15@gmail.com>
2023-08-25 13:50:39 +0700

    middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request })
     if (request.nextUrl.pathname.startsWith('/login')) {
