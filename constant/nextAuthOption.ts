7e7b9096f84a69c86e37c675d1cb1f1f1217e20c
RIZAL <achrizal15@gmail.com>
2023-08-25 13:20:14 +0700

    remove google

--- constant/nextAuthOption.ts
+++ constant/nextAuthOption.ts
@@ -1,14 +1,14 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import UserJwtInterface from "@/utils/Interfaces/UserJwtInterface";
 
 const nextAuthOptions: NextAuthOptions = {
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_CLIENT_ID,
+        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        // }),
         CredentialsProvider({
             id: 'credentials',
             name: 'Credentials',
