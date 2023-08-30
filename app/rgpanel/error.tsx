f88084e6f17cfe962d7cbaa82902f1c73daa8ecc
RIZAL <achrizal15@gmail.com>
2023-08-30 20:31:05 +0700

    Error page rgpanel;

---
+++ app/rgpanel/error.tsx
@@ -0,0 +1,28 @@
+'use client' // Error components must be Client Components
+
+import PrimeButton from '@/components/core/Button/PrimeButton'
+import { H2 } from '@/components/core/Headings/Headings'
+import Image from 'next/image'
+import erro500Image from 'public/images/error500.png'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  console.log(error.message)
+  return (
+    <div className='w-full h-full flex justify-center flex-col items-center mt-10'>
+      <Image src={erro500Image} alt='error Image' width={300} height={0} priority />
+      <div className='flex flex-col items-center gap-5'>
+        <H2 className='text-center'>Oops, Server Error</H2>
+        <p className='text-sm text-center'>Coba beberapa saat lagi, atau hubungi team renggani.</p>
+        <a href="">
+          <PrimeButton severity='help' rounded={false}>Refresh</PrimeButton>
+        </a>
+      </div>
+    </div>
+  )
+}
