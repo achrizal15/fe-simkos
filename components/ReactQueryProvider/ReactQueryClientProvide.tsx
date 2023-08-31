4c9fbbcd256fa99c9f7bca0d356d05cab1ff2924
RIZAL <achrizal15@gmail.com>
2023-08-31 16:49:22 +0700

    no refetch on tap

--- components/ReactQueryProvider/ReactQueryClientProvide.tsx
+++ components/ReactQueryProvider/ReactQueryClientProvide.tsx
@@ -4,10 +4,13 @@ import {
     QueryClientProvider
 } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
-const queryClient = new QueryClient()
-queryClient.defaultQueryOptions({
-    refetchOnWindowFocus: false
-});
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries:{
+            refetchOnWindowFocus: false
+        }
+    }
+})
 const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
     return (
         <QueryClientProvider client={queryClient}>
